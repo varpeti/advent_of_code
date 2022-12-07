fc1e7a82f44a292004618345cbdd4e70e61023e8
varpeti <peti.var97@gmail.com>
2022-12-07 22:03:24 +0100

    Day4 [100%] (🦀)

--- src/day4.rs
+++ src/day4.rs
@@ -11,7 +11,7 @@ impl Range {
             Range { s: e, e: s }
         }
     }
-    fn fully_contain(&self, oth: &Self) -> bool {
+    fn is_fully_contain(&self, oth: &Self) -> bool {
         if self.s >= oth.s && self.e <= oth.e {
             true
         } else if self.s <= oth.s && self.e >= oth.e {
@@ -20,30 +20,43 @@ impl Range {
             false
         }
     }
+    fn is_overlapping(&self, oth: &Self) -> bool {
+        self.s <= oth.e && self.e >= oth.s
+    }
+}
+
+fn elf_to_range(elf: &str) -> Range {
+    let mut range_iter = elf
+        .split("-")
+        .map(|num| num.parse::<u32>().expect("parse err elf"));
+    Range::new(
+        range_iter.next().expect("range error 0"),
+        range_iter.next().expect("range error 1"),
+    )
 }
 
 pub fn day4_a(input: &String) -> String {
     let sum = input
         .lines()
         .map(|pair| {
-            let mut pair_iter = pair.split(",").map(|elf| {
-                let mut range_iter = elf
-                    .split("-")
-                    .map(|num| num.parse::<u32>().expect("parse err elf"));
-                Range::new(
-                    range_iter.next().expect("range error 0"),
-                    range_iter.next().expect("range error 1"),
-                )
-            });
+            let mut pair_iter = pair.split(",").map(elf_to_range);
             let range_a = pair_iter.next().expect("pair error a");
             let range_b = pair_iter.next().expect("pair error b");
-            range_a.fully_contain(&range_b) as u32
+            range_a.is_fully_contain(&range_b) as u32
         })
         .sum::<u32>();
     format!("{}", sum)
 }
 
 pub fn day4_b(input: &String) -> String {
-    drop(input);
-    format!("b")
+    let sum = input
+        .lines()
+        .map(|pair| {
+            let mut pair_iter = pair.split(",").map(elf_to_range);
+            let range_a = pair_iter.next().expect("pair error a");
+            let range_b = pair_iter.next().expect("pair error b");
+            range_a.is_overlapping(&range_b) as u32
+        })
+        .sum::<u32>();
+    format!("{}", sum)
 }
