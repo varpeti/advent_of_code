17a398e3e355f8fa0d4d49e94a21fd07719c13ea
varpeti <peti.var97@gmail.com>
2022-12-13 22:45:27 +0100

    Day10 (🦀)

--- src/day10.rs
+++ src/day10.rs
@@ -1,10 +1,94 @@
+use std::str::Lines;
+
+const CYCLE_PERIOD: i32 = 40;
 
 pub fn day10_a(input: &String) -> String {
-    drop(input);
-    format!("a")
+    let mut sum: i128 = 0;
+    let mut x = 1;
+    let mut prev_x = 1;
+    let mut cycle = 1;
+    let mut last_cycle = -20;
+    for line in input.lines() {
+        match line {
+            _ if line.starts_with("addx") => {
+                prev_x = x;
+                x += line
+                    .split(" ")
+                    .last()
+                    .expect("add")
+                    .parse::<i32>()
+                    .expect("parse add");
+                cycle += 2;
+            }
+            _ if line.starts_with("noop") => {
+                cycle += 1;
+            }
+            err => panic!("Invaild input! {}", err),
+        }
+        if cycle == last_cycle + CYCLE_PERIOD {
+            last_cycle += CYCLE_PERIOD;
+            sum += (x * last_cycle) as i128;
+            continue;
+        }
+        if cycle > last_cycle + CYCLE_PERIOD {
+            last_cycle += CYCLE_PERIOD;
+            sum += (prev_x * last_cycle) as i128;
+        }
+    }
+    format!("{}", sum)
+}
+
+#[derive(Debug, Clone)]
+struct Task {
+    remaining_cycle: usize,
+    dx: i32,
+}
+
+fn get_next_task(lines: &mut Lines) -> Option<Task> {
+    match lines.next() {
+        Some(add) if add.starts_with("addx") => {
+            let dx = add
+                .split(" ")
+                .last()
+                .expect("add")
+                .parse::<i32>()
+                .expect("parse add");
+            Some(Task {
+                remaining_cycle: 2,
+                dx,
+            })
+        }
+        Some(nop) if nop.starts_with("noop") => Some(Task {
+            remaining_cycle: 1,
+            dx: 0,
+        }),
+        Some(err) => panic!("Invalid input! {}", err),
+        None => None,
+    }
 }
 
 pub fn day10_b(input: &String) -> String {
-    drop(input);
-    format!("b")
+    let mut reg_x = 1;
+    let mut lines = input.lines();
+    let mut current_task = get_next_task(&mut lines);
+    let mut s = String::new();
+    for _y in 0..6 {
+        for x in 0..40 {
+            if (reg_x - 1..reg_x + 2).contains(&x) {
+                print!("#");
+            } else {
+                s = format!("{}.", s);
+                print!(".");
+            }
+            if let Some(task) = &mut current_task {
+                task.remaining_cycle -= 1;
+                if task.remaining_cycle == 0 {
+                    reg_x += task.dx;
+                    current_task = get_next_task(&mut lines);
+                }
+            }
+        }
+        println!("");
+    }
+    format!("PRINTED_ANSWER")
 }
